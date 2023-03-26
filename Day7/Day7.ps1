0fe9686e8daa6f1e6d75e9a82d34bc41bd71f109
hack <hacktron86@gmail.com>
2023-03-26 00:50:12 -0500

    Day 7 progress

--- Day7/Day7.ps1
+++ Day7/Day7.ps1
@@ -7,38 +7,52 @@ function Start-PartOne {
             $fileContent = (Get-Content $fileName) -split "`n"
 
             $fileSystem = [List[PSCustomObject]]::new()
-            Add-Child -name "/" -type "d" -fileSystem ([ref]$fileSystem)
+
+            $currentName = [List[char]]::new()
+
+            $fileSystem.Add([PSCustomObject]@{Name="/";Type="d";Children=[List[PSCustomObject]]::new()})
+
+            $currentNav = $fileSystem
 
             foreach ($line in $fileContent) {
 
-                switch -Regex ($line) {
-# '\$\scd\s\/' {} # first line default
-                    '\$\scd\s(?:\w|\/)' { 
-                        Enter-Directory -name $(($_ -split " ")[2]) -fileSystem ([ref]$fileSystem.Children)
-                    } # cd into a directory
-                    '\$\scd\s\.\.' { Exit-Directory } # navigate up a directory
-# '\$\sls' {Get-Message} # list contents
-                        'dir\s\w' { Add-Child -name (($_ -split " ")[1]) -type "d" -filesystem ([ref]$fileSystem)} # add a sub directory
-                        '\d' { Add-Child -name (($_ -split " ")[1]) -type "f" -size (($_ -split " ")[0]) -fileSystem ([ref]$fileSystem)} # add a file
-                        default {} # default
-                }
 
+                Write-Verbose $line
+
+                    switch -Regex ($line) {
+                        '\$\scd\s\/' { 
+                            $currentName.Add(($_ -split " ")[2]) 
+                        }
+                        '\$\scd\s\w' { 
+                            $currentName.Add(($_ -split " ")[2]) 
+                                $currentNav = ($filesystem|Select-Object Children).Children | Where-Object { $_.Name -eq $currentName[-1]}
+                        }
+                        '\$\scd\s\.\.' { $currentName.Remove($currentName[-1]) } # navigate up a directory
+                    'dir\s\w' { Add-Child -name (($_ -split " ")[1]) -type "d" -filesystem $currentNav } # add a sub directory
+                    '\d' { Add-Child -name (($_ -split " ")[1]) -type "f" -size (($_ -split " ")[0]) -fileSystem $currentNav} # add a file
+                    default {} # default
             }
+
+    }
 
     return $fileSystem
 
 }
 
 function Enter-Directory {
-    param([string]$name,[ref]$fileSystem)
+    param([string]$name,[List[PSCustomObject]]$fileSystem)
 }
 
 function Add-Child {
-    param([string]$name,[string]$type,[int]$size,[ref]$fileSystem)
+    param([string]$name,[string]$type,[int]$size,[List[PSCustomObject]]$fileSystem)
 
+        Write-Verbose $name
+        Write-Verbose $name
+
+        $fileSystemToEdit = ($fileSystem | Select-Object Children).Children
         $null = switch ($type) {
             "d" {
-                $fileSystem.Value.Add(
+                $fileSystemToEdit.Add(
                         [PSCustomObject]@{
                         Name = $name
                         Type = $type
@@ -47,7 +61,7 @@ function Add-Child {
                         )
             }
             "f" {
-                $fileSystem.Value.Add(
+                $fileSystemToEdit.Add(
                         [PSCustomObject]@{
                         Name = $name
                         Type = $type
@@ -60,4 +74,20 @@ function Add-Child {
 }
 
 function Exit-Directory {
+}
+
+function Build-Directory {
+    param([string]$line,[List[PSCustomObject]]$fileSystem)
+
+        switch -Regex ($line) {
+            '\$\scd\s(?:\w|\/)' { ($_ -split " ")[2] }
+#     Build-Directory -fileSystem ($fileSystem | Where-Object { $_.Name -eq $(($_ -split " ")[2]) })
+# } # cd into a directory
+    '\$\scd\s\.\.' { Exit-Directory } # navigate up a directory
+# '\$\sls' {Get-Message} # list contents
+        'dir\s\w' { Add-Child -name (($_ -split " ")[1]) -type "d" -filesystem $fileSystem} # add a sub directory
+        '\d' { Add-Child -name (($_ -split " ")[1]) -type "f" -size (($_ -split " ")[0]) -fileSystem $fileSystem} # add a file
+        default {} # default
+}
+
 }
