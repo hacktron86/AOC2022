dd0b2fecef363ba077e43212b46e096289f8ac36
hack <hacktron86@gmail.com>
2023-04-05 22:40:29 -0500

    Day 8 idk

--- Day8/Day8.ps1
+++ Day8/Day8.ps1
@@ -2,42 +2,57 @@ using namespace System.Collections.Generic
 
 function Invoke-Main {
 
-    [List[Int[]]]$list = Build-List -filename "testinput.txt"
+    $list = Build-List -filename "testinput.txt"
 
-    return $list
-
-}
-
-function Build-List([string]$filename) {
-    [OutputType([List[Int[]]])]
-
-    $list = [List[int[]]]::new()
-
-    $lineInput = (Get-Content $filename) -split "`n"
-
-    foreach ($line in $lineInput) {
-
-        $lineArray = $line.ToCharArray() | ForEach-Object { [convert]::ToInt32($_, 10) }
-
-        $list.Add($lineArray)
-
-    }
-
-    $visibleTrees = New-Object 'object[,]' $list.Count,$list[0].Count
+    $visibleTrees = New-Object 'object[,]' $list[0].Count, $list[0][0].Count
 
     Invoke-TreeVisibilityFunction -list $list -visible $visibleTrees
-
+    
     return $null
 
 }
 
-function Invoke-TreeVisibilityFunction ([List[Int[]]]$list,$visibleTrees) {
+function Build-List([string]$filename) {
 
-    for ( $y = 1; $y -lt ( $list.Count - 1 ); $y++ ) {
+    $hList = [List[int[]]]::new()
 
-        for ( $x = 1; $x -lt ( $list[$y].Count - 1 ); $x++) {
+    $lineInput = (Get-Content $filename) -split "`n"
 
-            Write-Information $list[$y][$x] -InformationAction Continue
+    $null = foreach ($line in $lineInput) {
+
+        $lineArray = $line.ToCharArray() | ForEach-Object { [convert]::ToInt32($_, 10) }
+
+        $hList.Add($lineArray)
+
+    }
+
+    $vList = [List[int[]]]::new()
+
+    for ( $x = 0; $x -lt $hList[0].Count; $x++) {
+
+        $arr = @()
+
+        for ( $y = 0; $y -lt $hList.Count; $y++ ) {
+            
+            $arr += $hList[$y][$x]    
+
+        }
+
+        $vList.Add($arr)
+
+    }
+
+    return @($hList, $vList)
+
+}
+
+function Invoke-TreeVisibilityFunction ($list, $visibleTrees) {
+
+    for ( $y = 1; $y -lt ( $list[0].Count - 1 ); $y++ ) {
+
+        for ( $x = 1; $x -lt ( $list[0][$y].Count - 1 ); $x++) {
+
+            #Write-Information $list[0][$y][$x] -InformationAction Continue
 
             Search-AllDirections -y $y -x $x -list $list -visible $visibleTrees
 
@@ -51,31 +66,32 @@ function Invoke-TreeVisibilityFunction ([List[Int[]]]$list,$visibleTrees) {
 function Search-AllDirections ($y, $x, $list, $visibleTrees) {
 
     # check up
-    if ( $list[$y][$x] -gt $list[$y-1][$x] ) {
-        Search-Direction -y $y -x $x -list $list -visible $visibleTrees -direction "up"
+    if ( $list[0][$y][$x] -gt $list[0][$y - 1][$x] ) {
+        Search-Direction -y $y -x $x -list $list[1][$x] -visible $visibleTrees
     }
     # check down
-    if ( $list[$y][$x] -gt $list[$y+1][$x] ) {
-        $true
+    if ( $list[0][$y][$x] -gt $list[0][$y + 1][$x] ) {
+        Search-Direction -y $y -x $x -list [array]::Reverse($list[1][$x]) -visible $visibleTrees
     }
     # check left    
-    if ( $list[$y][$x] -gt $list[$y][$x-1] ) {
-        $true
+    if ( $list[0][$y][$x] -gt $list[0][$y][$x - 1] ) {
+        Search-Direction -y $y -x $x -list $list[0][$y] -visible $visibleTrees
     }
     # check right
-    if ( $list[$y][$x] -gt $list[$y][$x+1] ) {
-        $true
+    if ( $list[0][$y][$x] -gt $list[0][$y][$x + 1] ) {
+        Search-Direction -y $y -x $x -list [array]::Reverse($list[0][$y]) -visible $visibleTrees
     }
     return
 
 }
 
-function Search-Direction ($y, $x, $list, $visibleTrees, $direction) {
+function Search-Direction ($y, $x, $list, $visibleTrees) {
 
-    switch ($direction) {
-        "up" {  }
-        Default {}
+    for ( $i = 1; $i -lt ($list.count - 1); $i++) {
+
     }
+
+    return $true    
 
 }
 
